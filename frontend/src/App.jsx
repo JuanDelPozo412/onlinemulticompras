b78fbe039ebf24030d697f233f649abee54e2b7a
Juan Del Pozo <juandelpozo412@gmail.com>
2026-02-05 17:40:26 -0300

    Commit Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 //pages
 import { Home } from './pages/Home';
-import { Login } from './pages/Login';
-import { Register } from './pages/Register';
-import {Products} from './pages/Products';
 
-export function App() {
+
+ function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/products" element={<Products />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
+export default App;
